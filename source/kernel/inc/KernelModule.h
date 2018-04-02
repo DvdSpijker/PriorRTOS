1afe28ac2acddc403cbf518a66c9c9e5c5ca1293
D. van de Spijker <spijker.dorus@gmail.com>
2018-04-02 11:33:55 +0200

    + KernelModule : OS frequency change callback type.

--- source/kernel/inc/KernelModule.h
+++ source/kernel/inc/KernelModule.h
@@ -12,10 +12,11 @@ typedef enum {
 } KernelModuleState_t;
 
 /* Kmf = Kernel Module Function. */
-typedef OsResult_t	(*KmfInit)(const void *args);
-typedef U32_t		(*KmfObjectToString)(void *obj, char *obj_str);
-typedef OsResult_t	(*KmfHookException)(U32_t exception);
-typedef void 		(*KmfHookTick)(void);
+typedef OsResult_t	(*KmfGeneric_t)(void *context);
+typedef U32_t		(*KmfObjectToString_t)(void *obj, char *obj_str);   /* Converts the passed object (obj) to a string (obj_str), the lenght of the string is returned. */
+typedef OsResult_t	(*KmfHookError_t)(U32_t err_code);
+typedef void 		(*KmfHookTick_t)(void);
+typedef void		(*KmfOsFreqChange)(uint16_t old_freq, uint16_t new_freq);
 
 struct KernelModule {
 	const char name[];
@@ -27,11 +28,12 @@ struct KernelModule {
 	
 	const struct LinkedList_t *lists[];
 	
-	KmfInit 			init;
-	KmfInit 			deinit;
-	KmfHookException 	hook_exception;
-	KmfHookTick 		hook_tick;
-	KmfObjectToString 	object_to_string;
+	KmfGeneric_t        init;
+	KmfGeneric_t		deinit;
+	KmfHookError_t      on_error;
+	KmfHookTick_t 		on_tick;
+	KmfOsFreqChange		on_freq_change;
+	KmfObjectToString_t object_to_string;
 };
 
 
