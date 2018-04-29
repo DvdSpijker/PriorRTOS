083e96a782598d6e17d3ac682b9aac7b69bb2528
D. van de Spijker <spijker.dorus@gmail.com>
2018-04-29 17:11:20 +0200

    + PortLogger : Implementation PortDebugWriteString.

--- ports/STMicroelectronics/STM32F1xx/port/PortLogger.c
+++ ports/STMicroelectronics/STM32F1xx/port/PortLogger.c
@@ -10,23 +10,43 @@
 #include "usart.h"
 #include "stm32f1xx_hal_uart.h"
 
+#include <string.h>
+
 int PortDebugUartInit(uint32_t baud_rate)
 {
 	 MX_USART1_UART_Init(baud_rate);
+
+	 return 0;
 }
 
 int PortDebugUartWriteString(char *str)
 {
+#define MAX_STRING_LENGTH 1000
+
+	size_t str_len = strnlen((const char *)str, MAX_STRING_LENGTH + 1);
+
+	for(size_t i = 0; i < str_len; i++)
+	{
+		if(PortDebugUartWriteChar(str[i]) == -1) {
+			break;
+		}
+	}
+
+	return (int)str_len;
 }
 
 int PortDebugUartWriteChar(char c)
 {
-	HAL_UART_Transmit(&huart1, (uint8_t *)&c, 1, 100);
+	HAL_StatusTypeDef res =	HAL_UART_Transmit(&huart1, (uint8_t *)&c, 1, 100);
+
+	return (res == HAL_OK ? 0 : -1);
 }
 
 char PortDebugUartReadChar(void)
 {
 	char c = '\0';
+
 	HAL_UART_Receive(&huart1, &c, 1, 100);
+
 	return c;
 }
