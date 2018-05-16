caaa5846460ebd0b928447efd3177d3dbe14b673
D. van de Spijker <spijker.dorus@gmail.com>
2018-05-16 21:31:22 +0200

    + IdTypeDef : Header containing kernel-only ID functions.,

---
+++ source/kernel/inc/IdTypeDef.h
@@ -0,0 +1,17 @@
+/*
+ * IdTypeDef.h
+ *
+ *  Created on: 16 mei 2018
+ *      Author: Dorus
+ */
+
+#ifndef ID_TYPE_DEF_H_
+#define ID_TYPE_DEF_H_
+
+/* Initialize ID pools. */
+void KIdInit(void);
+
+/* Request an ID of a specific type. */
+Id_t KIdRequest(IdGroup_t type);
+
+#endif /*ID_TYPE_DEF_H_ */
