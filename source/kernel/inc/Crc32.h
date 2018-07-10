7bd4f2c0f947ff7e387a09f7b1a15216538d0767
D. van de Spijker <spijker.dorus@gmail.com>
2018-07-10 09:50:34 +0200

    + kernel/Crc32 : CRC32 implementation.

---
+++ source/kernel/inc/Crc32.h
@@ -0,0 +1,15 @@
+/*
+ * Crc32.h
+ *
+ *  Created on: 10 jul. 2018
+ *      Author: Dorus
+ */
+
+#ifndef CRC32_H_
+#define CRC32_H_
+
+#include "StdTypes.h"
+
+U32_t Crc32 (U32_t prev_crc, const void *buf, U32_t len);
+
+#endif /* CRC32_H_ */
