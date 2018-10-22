fd294c3accd58326e31fbd4d8f84cfd341fed451
D. van de Spijker <spijker.dorus@gmail.com>
2018-10-22 22:10:53 +0200

    + tests/test_IdType : Tests for IdListCount, IdListCopy.
    * tests/test_IdType : Updated tests.

--- tests/test_IdType.c
+++ tests/test_IdType.c
@@ -3,8 +3,8 @@
 #include "IdType.h"
 #include "IdTypeDef.h"
 
-int set_up(void);
-int tear_down(void);
+static int set_up(void);
+static int tear_down(void);
 
 int test_KIdRequest(void);
 int test_KIdRequest_invalid_group(void);
@@ -42,13 +42,23 @@ int test_IdListIdAdd_all_plus_one(void);
 int test_IdListIdRemove(void);
 int test_IdListIdRemove_invalid_list(void);
 int test_IdListIdRemove_single(void);
+int test_IdListIdRemove_all(void);
+int test_IdListIdRemove_all_plus_one(void);
 
+int test_IdListCount(void);
+int test_IdListCount_invalid_list(void);
+int test_IdListCount_single(void);
+int test_IdListCount_all(void);
+
+int test_IdListCopy(void);
+int test_IdListCopy_invalid_lists(void);
+int test_IdListCopy_valid_lists(void);
 
 ut_test_set_t test_set_IdType = {
 	.name = "IdType",
 	.set_up = set_up,
 	.tear_down = tear_down,
-	.n_tests = 7,
+	.n_tests = 9,
 	.tests = {
 	test_KIdRequest,
 	test_IdSequenceNumberGet,
@@ -56,17 +66,19 @@ ut_test_set_t test_set_IdType = {
 	test_IdIsInGroup,
 	test_IdListInit,
 	test_IdListIdAdd,
-	test_IdListIdRemove
+	test_IdListIdRemove,
+	test_IdListCount,
+	test_IdListCopy
 	},
 };
 
-int set_up(void)
+static int set_up(void)
 {
 	KIdInit();
 	ut_end;
 }
 
-int tear_down(void) 
+static int tear_down(void) 
 {
 	ut_end;
 }
@@ -419,7 +431,7 @@ int test_IdListIdAdd_all(void)
 	for(uint8_t j = 0; j < ID_LIST_SIZE_MAX; j++) {
 		id = j;
 		IdListIdAdd(&list, id);
-		ut_assert_eq(list.n, j + 1, "Expected number of items in the ID list incorrect.");
+		ut_assert_eq(list.n, j + 1, "Number of items in the ID list incorrect.");
 		ut_assert_eq(list.ids[list.n - 1], id, "ID in the list is incorrect.");
 	}
 	ut_assert_eq(list.n, ID_LIST_SIZE_MAX, "Number of IDs in the list is incorrect");
@@ -459,8 +471,8 @@ int test_IdListIdRemove(void)
 {
 	ut_run_test(test_IdListIdRemove_invalid_list);
 	ut_run_test(test_IdListIdRemove_single);
-	//ut_run_test(test_IdListIdAdd_all);
-	//ut_run_test(test_IdListIdAdd_all_plus_one);
+	ut_run_test(test_IdListIdRemove_all);
+	ut_run_test(test_IdListIdRemove_all_plus_one);
 	
 	ut_end;
 }
@@ -494,6 +506,225 @@ int test_IdListIdRemove_single(void)
 	Id_t rm_id = IdListIdRemove(&list);
 	ut_assert_eq(id, rm_id, "Removed ID not equal to added ID.");
 	ut_assert_eq(list.n, 0, "Number of items in the list incorrect.");
+	
+	ut_end;
+}
+
+int test_IdListIdRemove_all(void)
+{
+	ut_start;
+	
+	Id_t id_array[ID_LIST_SIZE_MAX] = {ID_INVALID};
+	Id_t id = 1;
+	IdList_t list;
+	IdListInit(&list);
+	ut_assert_eq(list.n, ID_LIST_EMPTY, "List count was not initialized to ID_LIST_EMPTY.");
+	for(uint8_t i = 0; i < ID_LIST_SIZE_MAX; i++) {
+		ut_assert_eq(list.ids[i], ID_INVALID, "List IDs were not all initialized to ID_INVALID.");
+	}
+	
+	for(uint8_t i = 0; i < ID_LIST_SIZE_MAX; i++) {
+		id = i;
+		id_array[i] = id;
+		IdListIdAdd(&list, id);
+		ut_assert_eq(list.n, i + 1, "Number of items in the ID list incorrect.");
+		ut_assert_eq(list.ids[list.n - 1], id, "ID in the list is incorrect.");
+	}
+	ut_assert_eq(list.n, ID_LIST_SIZE_MAX, "Number of IDs in the list is incorrect");
+	
+	
+	Id_t rm_id = ID_INVALID;
+	for(uint8_t i = 0; i < ID_LIST_SIZE_MAX; i++) {
+		rm_id = IdListIdRemove(&list);
+		ut_assert_eq(id_array[ID_LIST_SIZE_MAX - (i + 1)], rm_id, "Removed ID not equal to added ID.");
+		ut_assert_eq(list.n, ID_LIST_SIZE_MAX - (i + 1), "Number of items in the list incorrect.");
+	}
+	ut_assert_eq(list.n, 0, "Number of IDs in the list is not 0.");
+	
+	ut_end;
+}
+
+int test_IdListIdRemove_all_plus_one(void)
+{
+	ut_start;
+	
+	Id_t id_array[ID_LIST_SIZE_MAX] = {ID_INVALID};
+	Id_t id = 1;
+	IdList_t list;
+	IdListInit(&list);
+	ut_assert_eq(list.n, ID_LIST_EMPTY, "List count was not initialized to ID_LIST_EMPTY.");
+	for(uint8_t i = 0; i < ID_LIST_SIZE_MAX; i++) {
+		ut_assert_eq(list.ids[i], ID_INVALID, "List IDs were not all initialized to ID_INVALID.");
+	}
+	
+	for(uint8_t i = 0; i < ID_LIST_SIZE_MAX; i++) {
+		id = i;
+		id_array[i] = id;
+		IdListIdAdd(&list, id);
+		ut_assert_eq(list.n, i + 1, "Number of items in the ID list incorrect.");
+		ut_assert_eq(list.ids[list.n - 1], id, "ID in the list is incorrect.");
+	}
+	ut_assert_eq(list.n, ID_LIST_SIZE_MAX, "Number of IDs in the list is incorrect");
+	
+	
+	Id_t rm_id = ID_INVALID;
+	for(uint8_t i = 0; i < ID_LIST_SIZE_MAX; i++) {
+		rm_id = IdListIdRemove(&list);
+		ut_assert_eq(id_array[ID_LIST_SIZE_MAX - (i + 1)], rm_id, "Removed ID not equal to added ID.");
+		ut_assert_eq(list.n, ID_LIST_SIZE_MAX - (i + 1), "Number of items in the list incorrect.");
+	}
+	ut_assert_eq(list.n, 0, "Number of IDs in the list is not 0.");
+	
+	rm_id = IdListIdRemove(&list);
+	ut_assert_eq(rm_id, ID_INVALID, "ID removed from empty list.");
+	
+	ut_end;
+}
+
+/***** Test IdListCount *****/
+
+int test_IdListCount(void)
+{
+	ut_run_test(test_IdListCount_invalid_list);
+	ut_run_test(test_IdListCount_single);
+	ut_run_test(test_IdListCount_all);
+	
+	ut_end;
+}
+
+int test_IdListCount_invalid_list(void)
+{
+	ut_start;
+	
+	U8_t cnt = IdListCount(NULL);
+	ut_assert_eq(cnt, ID_LIST_EMPTY, "Valid count returned for an invalid list.");
+	
+	ut_end;
+}
+
+int test_IdListCount_single(void)
+{
+	ut_start;
+	
+	Id_t id = 1;
+	IdList_t list;
+	IdListInit(&list);
+	ut_assert_eq(list.n, ID_LIST_EMPTY, "List count was not initialized to ID_LIST_EMPTY.");
+	for(uint8_t i = 0; i < ID_LIST_SIZE_MAX; i++) {
+		ut_assert_eq(list.ids[i], ID_INVALID, "List IDs were not all initialized to ID_INVALID.");
+	}
+	
+	U8_t cnt = IdListCount(&list);
+	ut_assert_eq(cnt, ID_LIST_EMPTY, "Non-empty count returned for an empty list.");
+	
+	IdListIdAdd(&list, id);
+	ut_assert_eq(list.n, 1, "Expected 1 item in the ID list.");
+	ut_assert_eq(list.ids[list.n - 1], id, "ID in the list is not correct.");
+	
+	cnt = IdListCount(&list);
+	ut_assert_eq(cnt, 1, "List count incorrect.");
+	
+	Id_t rm_id = IdListIdRemove(&list);
+	ut_assert_eq(id, rm_id, "Removed ID not equal to added ID.");
+	ut_assert_eq(list.n, 0, "Number of items in the list incorrect.");
+	
+	cnt = IdListCount(&list);
+	ut_assert_eq(cnt, ID_LIST_EMPTY, "Non-empty count returned for an empty list.");
+	
+	ut_end;
+}
+
+int test_IdListCount_all(void)
+{
+	ut_start;
+	
+	Id_t id_array[ID_LIST_SIZE_MAX] = {ID_INVALID};
+	Id_t id = 1;
+	IdList_t list;
+	U8_t cnt = ID_LIST_EMPTY;
+	IdListInit(&list);
+	ut_assert_eq(list.n, ID_LIST_EMPTY, "List count was not initialized to ID_LIST_EMPTY.");
+	for(uint8_t i = 0; i < ID_LIST_SIZE_MAX; i++) {
+		ut_assert_eq(list.ids[i], ID_INVALID, "List IDs were not all initialized to ID_INVALID.");
+	}
+	
+	cnt = IdListCount(&list);
+	ut_assert_eq(cnt, ID_LIST_EMPTY, "Non-empty count returned for an empty list.");
+	
+	for(uint8_t i = 0; i < ID_LIST_SIZE_MAX; i++) {
+		id = i;
+		id_array[i] = id;
+		IdListIdAdd(&list, id);
+		ut_assert_eq(list.n, i + 1, "Number of items in the ID list incorrect.");
+		ut_assert_eq(list.ids[list.n - 1], id, "ID in the list is incorrect.");
+		cnt = IdListCount(&list);
+		ut_assert_eq(cnt, i+1, "List count incorrect.");
+	}
+	ut_assert_eq(list.n, ID_LIST_SIZE_MAX, "Number of IDs in the list is incorrect");
+	cnt = IdListCount(&list);
+	ut_assert_eq(cnt, ID_LIST_SIZE_MAX, "List count incorrect.");
+	
+	Id_t rm_id = ID_INVALID;
+	for(uint8_t i = 0; i < ID_LIST_SIZE_MAX; i++) {
+		rm_id = IdListIdRemove(&list);
+		ut_assert_eq(id_array[ID_LIST_SIZE_MAX - (i + 1)], rm_id, "Removed ID not equal to added ID.");
+		ut_assert_eq(list.n, ID_LIST_SIZE_MAX - (i + 1), "Number of items in the list incorrect.");
+		cnt = IdListCount(&list);
+		ut_assert_eq(cnt, ID_LIST_SIZE_MAX - (i + 1), "List count incorrect.");
+	}
+	ut_assert_eq(list.n, 0, "Number of IDs in the list is not 0.");
+	cnt = IdListCount(&list);
+	ut_assert_eq(cnt, ID_LIST_EMPTY, "Non-empty count returned for an empty list.");
+	
+	ut_end;
+}
+
+
+/***** Test IdListCopy *****/
+
+int test_IdListCopy(void)
+{
+	ut_run_test(test_IdListCopy_invalid_lists);
+	ut_run_test(test_IdListCopy_valid_lists);
+	
+	ut_end;
+}
+
+int test_IdListCopy_invalid_lists(void)
+{
+	ut_start;
+	
+	ut_end;
+}
+
+int test_IdListCopy_valid_lists(void)
+{
+	ut_start;
+	
+	Id_t id = 1;
+	IdList_t list;
+	IdListInit(&list);
+	ut_assert_eq(list.n, ID_LIST_EMPTY, "List count was not initialized to ID_LIST_EMPTY.");
+	for(uint8_t i = 0; i < ID_LIST_SIZE_MAX; i++) {
+		ut_assert_eq(list.ids[i], ID_INVALID, "List IDs were not all initialized to ID_INVALID.");
+	}
+	
+	for(uint8_t j = 0; j < ID_LIST_SIZE_MAX; j++) {
+		id = j;
+		IdListIdAdd(&list, id);
+		ut_assert_eq(list.n, j + 1, "Number of items in the ID list incorrect.");
+		ut_assert_eq(list.ids[list.n - 1], id, "ID in the list is incorrect.");
+	}
+	ut_assert_eq(list.n, ID_LIST_SIZE_MAX, "Number of IDs in the list is incorrect");
+	
+	IdList_t list_cpy;
+	IdListInit(&list_cpy);
+	
+	IdListCopy(&list_cpy, &list);
+	ut_assert_eq(list.n, list_cpy.n, "Item count is not equal after copy.");
+	for(uint8_t k = 0; k < ID_LIST_SIZE_MAX; k++) {
+		ut_assert_eq(list.ids[k], list_cpy.ids[k], "IDs are not equal after copy.");
+	}
 	
 	ut_end;
 }
