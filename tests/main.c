4ee69c8338191f16f5ab92207d3f73c9b8dbb8ba
D. van de Spijker <spijker.dorus@gmail.com>
2018-10-22 22:14:38 +0200

    + tests/main : Run List test set.

--- tests/main.c
+++ tests/main.c
@@ -3,12 +3,14 @@
 #include "utest.h"
 
 #include "test_IdType.h"
+#include "test_List.h"
 
 int main(int argc, char **argv)
 {
 
 	ut_init();
 	ut_run_set(&test_set_IdType);
+	ut_run_set(&test_set_List);
 	ut_suite_report;
 
 	while(1);
